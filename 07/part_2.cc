b9cc4a6043fffc0874b5c5a9ef77551647697a69
Derek T. Jones <dtj@mac.com>
2021-12-11 09:23:00 -0800

    Day 7

---
+++ 07/part_2.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+#include <valarray>
+#include <map>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    string line; getline(cin, line);
+    transform(line.begin(), line.end(), line.begin(), [](char ch) {return ch == ',' ? ' ' : ch;});
+    istringstream ss(line);
+    vector<int> crab_hpos;
+    copy(istream_iterator<int>(ss), istream_iterator<int>(), back_inserter(crab_hpos));
+
+    valarray<int> orig(&crab_hpos[0], crab_hpos.size());
+    int c_begin = orig.min(), c_end = orig.max();
+    map<int, int> ranked;
+    for (int p = c_begin; p <= c_end; ++p) {
+        int diff = (orig - p).apply(abs).apply([] (int n) { return (n * (n + 1)) / 2; }).sum();
+        ranked[diff] = p;
+    }
+    cout << "Winning position: " << ranked.begin()->second 
+         << " using " << ranked.begin()->first << " fuel" << endl;
+}
