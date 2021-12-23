aa5c8dc62f6e358424641ab8b46de6aee9853edf
Derek T. Jones <dtj@mac.com>
2021-12-22 23:06:47 -0800

    Day 15, part 2

--- 15/part_2.cc
+++ 15/part_2.cc
@@ -3,6 +3,7 @@
 #include <iterator>
 #include <string>
 #include <vector>
+#include <list>
 #include <algorithm>
 
 using namespace std;
@@ -16,18 +17,61 @@ int m(int r, int c) {
     return r * cols + c;
 }
 
+void dump(string message) {
+    cout << message << endl;
+    for (int r = 0; r < rows; r++) {
+        for (int c = 0; c < cols; c++) {
+            int cursor = risks[m(r, c)];
+            cout << cursor;
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
+
 int main() {
+    // The grid must be expanded 5X in both directions, with
+    // the new value of each repeated tile (in each direction) being
+    // given as t2 = (t % 9) + 1.
+    //
+    // It may be easier to do this expansion right at the moment of reading.
+    list<vector<int>> row_list;
     string line; while(getline(cin, line)) {
         if (cols) {
             assert(cols == line.size());
         } else {
             cols = line.size();
         }
+        vector<int> risk_row(cols * 5);  // pre-size so iterators will be stable
+        transform(begin(line), end(line), begin(risk_row), [](int c) { return c - '0'; });
+        auto p_beg = begin(risk_row);
+        auto p_end = p_beg + line.size();
+        for (int s = 1; s < 5; s++) {
+            auto next_begin = p_end;
+            transform(p_beg, p_end, p_end, [](int t) { return (t % 9) + 1; });
+            p_beg = next_begin;
+            p_end = p_end + line.size();
+        }
+        row_list.push_back(risk_row);
         rows++;
-        transform(line.begin(), line.end(), back_inserter(risks), [](int c) {return c - '0';});
     }
+    cols *= 5;
+    // Now, prepare the whole matrix.  We start with one copy of the rows as they are,
+    // and then roll down four times, doing the mod-math to each.
+    for (int rrows = 0; rrows < 5; ++rrows) {
+        for (int ir = 0; ir < rows; ir++) {
+            copy(row_list.front().begin(), row_list.front().end(), back_inserter(risks));
+            row_list.push_back(vector<int>(cols));
+            transform(row_list.front().begin(), row_list.front().end(),
+                        row_list.back().begin(), [](int t) { return (t % 9) + 1; });
+            row_list.pop_front();
+        }
+    }
+    rows *= 5;
+    dump("After initial input:");
     SHOW(rows);
     SHOW(cols);
+    SHOW(risks.size());
     assert(rows == cols);
     vector<int> nr = risks;
     int iteration = 0;
